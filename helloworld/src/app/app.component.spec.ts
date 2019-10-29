50afbd1d6106be710964f5ec2baf116dfb571420
akitiainen <aki.tiainen@hotmail.fi>
2019-10-29 10:41:42 +0200

    helloworld initialized

--- helloworld/src/app/app.component.spec.ts
+++ helloworld/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TESTIPROJEKTI'`, () => {
+  it(`should have as title 'helloworld'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TESTIPROJEKTI');
+    expect(app.title).toEqual('helloworld');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TESTIPROJEKTI!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to helloworld!');
   });
 });
--- helloworld/src/karma.conf.js
+++ helloworld/src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/TESTIPROJEKTI'),
+      dir: require('path').join(__dirname, '../coverage/helloworld'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
