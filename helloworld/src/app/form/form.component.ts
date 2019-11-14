eb205fa5339197229d6b4ff191b866562a3f6686
akitiainen <aki.tiainen@hotmail.fi>
2019-11-14 15:31:15 +0200

    work on form component
    added new fields and validators

--- helloworld/src/app/form/form.component.ts
+++ helloworld/src/app/form/form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Person} from './person';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {first} from 'rxjs/operators';
 
 @Component({
@@ -12,9 +12,13 @@ export class FormComponent implements OnInit {
 
   person: Person;
 
-  form = new FormGroup({
-    firstName: new FormControl('')
-  });
+  /*form = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    termsAndConditions: new FormControl(Validators.required)
+  });*/
 
   constructor() {
     this.person = new Person();
@@ -23,6 +27,8 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSave() {}
+  onSave(form) {
+    console.log(form.value.firstName);
+  }
 
 }
