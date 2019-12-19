6a87c0874b147629909204cd9fc793f829721b3e
akitiainen <aki.tiainen@hotmail.fi>
2019-12-19 21:07:42 +0200

    added a swap button
    converter now converts with every change without pressing additional buttons

--- helloworld/src/app/currency-converter/currency-converter.component.ts
+++ helloworld/src/app/currency-converter/currency-converter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CurrencyConverterService} from '../services/currency-converter.service';
 import {Currency} from './currency';
 
@@ -13,40 +13,51 @@ export class CurrencyConverterComponent implements OnInit {
   baseCurrency: string;
   newCurrency: string;
   baseValue: number;
-  newValue: number;
+  newValue: string;
+  rate: number;
 
   constructor(private currencyService: CurrencyConverterService) {
+    this.baseCurrency = 'EUR';
+    this.newCurrency = 'USD';
   }
 
   ngOnInit() {
     this.getCurrencies();
   }
 
-  setBase(base) {
-    this.baseCurrency = base;
-  }
-
-  setSymbol(symbol) {
-    this.newCurrency = symbol;
-  }
-
   getCurrencies() {
     this.currencyService.getCurrencies().subscribe(response => {
       this.currencies = Object.keys(response.rates);
+      console.log(this.currencies);
       this.currencies.unshift('EUR');
     });
   }
 
   getRates() {
-    this.currencyService.get(this.baseCurrency, this.newCurrency).subscribe(response => {
-      this.currency = response;
-      console.log(this.currency);
-      this.CalculateRate();
-    });
+    if (this.baseCurrency === this.newCurrency) {
+      this.newValue = this.baseValue.toString();
+    } else {
+      this.currencyService.get(this.baseCurrency, this.newCurrency).subscribe(response => {
+        this.currency = response;
+        console.log(response);
+        this.CalculateRate();
+      });
+    }
+  }
+
+  swapCurrencies() {
+    const tmpCurrency = this.newCurrency;
+    this.newCurrency = this.baseCurrency;
+    this.baseCurrency = tmpCurrency;
+    this.rate = 1 / this.rate;
+    if (this.baseValue === undefined) {
+    } else {
+      this.newValue = (this.baseValue * this.rate).toFixed(2);
+    }
   }
 
   CalculateRate() {
-    const rate = Number(Object.values(this.currency.rates));
-    this.newValue = this.baseValue * rate;
+    this.rate = Number(Object.values(this.currency.rates));
+    this.newValue = (this.baseValue * this.rate).toFixed(2);
   }
 }
